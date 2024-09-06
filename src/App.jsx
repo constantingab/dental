11c9adbe4e5673116eb4109818166ebc7d819edc
Konstantin Gabelok <constantingab@gmail.com>
2024-09-06 15:27:51 +0300

    before add mobile menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Привет</h1>
       <div className="pt-[4.75] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login">Something</Button>
+        <Header />
       </div>
       <ButtonGradient />
     </>
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import brainwave from "./brainwave.svg";
+import dentalland from "./dentalland.svg";
 import check from "./check.svg";
 import brainwaveSymbol from "./brainwave-symbol.svg";
 import brainwaveWhiteSymbol from "./brainwave-symbol-white.svg";
@@ -76,7 +76,7 @@ import telegram from "./socials/telegram.svg";
 import twitter from "./socials/twitter.svg";
 
 export {
-  brainwave,
+  dentalland,
   check,
   check2,
   loading1,
