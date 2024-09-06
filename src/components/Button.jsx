ce07d58f1a4493064490aa14bb901718317aa400
Konstantin Gabelok <constantingab@gmail.com>
2024-09-06 10:45:16 +0300

    Button

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import ButtonSvg from "../assets/svg/ButtonSvg";
+
+const Button = ({ className, href, onclick, children, px, white }) => {
+  const renderButton = () => (
+    <button>
+      <span>{children}</span>
+      {ButtonSvg(white)}
+    </button>
+  );
+  return renderButton();
+};
+
+export default Button;
