6fef75d577c1810b0fdd7bcfd1789212bd0f9a70
Konstantin Gabelok <constantingab@gmail.com>
2024-09-16 17:20:03 +0300

    160924

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,18 @@
+import Section from "./Section";
+import Heading from "./Heading";
+
+const Benefits = () => {
+  return (
+    <Section id="futures">
+      <div className="container relative z-2 ">
+        <Heading
+          className="md:max-w-md lg:max-w-2xl"
+          title="Chat Smarter, Not Harder with Brainwave"
+        />
+        <div className="flex flex-wrap gap-10 mb-10">{}</div>
+      </div>
+    </Section>
+  );
+};
+
+export default Benefits;
