12e03552f50300edf684aed909766d2b7a150348
Konstantin Gabelok <constantingab@gmail.com>
2024-09-16 18:00:50 +0300

    remove some sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
-import Collaboration from "./components/Collaboration";
+
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
-import Roadmap from "./components/Roadmap";
 
 const App = () => {
   return (
@@ -14,9 +13,8 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
-        <Collaboration />
+
         <Pricing />
-        <Roadmap />
         <Footer />
       </div>
       <ButtonGradient />
