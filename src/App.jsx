f1b05e086636c2bd35cc8e8385876f92c5c15a22
Konstantin Gabelok <constantingab@gmail.com>
2024-09-16 17:39:42 +0300

    Add Collaboration

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -12,6 +13,7 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
         <Roadmap />
         <Footer />
       </div>
